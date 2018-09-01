c968eb3ce988f0bcc32a51a36b985db1896afdd0
Malcolm Jones <malcolm@adobe.com>
2018-09-01 18:00:20 -0400

    wip: manifest

--- .ci/docker-run.sh
+++ .ci/docker-run.sh
@@ -7,7 +7,7 @@ _DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TAG="bossjones/scarlett-jhbuild:latest"
 
 docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
-    --file "Dockerfile.ci" ../.
+    --file "Dockerfile.ci" $(pwd)
 
 docker run -e PYENV_VERSION='3.7.0-debug' --rm --security-opt label=disable \
     --volume "$(pwd)/:/home/developer/app" --workdir "/home/developer/app" \
