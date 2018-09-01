ef1329e8b57cca48bdb5b9ba361d3e1ad38e412c
Malcolm Jones <malcolm@adobe.com>
2018-09-01 18:37:48 -0400

    wip: manifest

--- .ci/docker-run.sh
+++ .ci/docker-run.sh
@@ -12,11 +12,3 @@ docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
 docker run -e PYENV_VERSION='3.7.0-debug' --rm --security-opt label=disable \
     --volume "$(pwd)/:/home/developer/app" --workdir "/home/developer/app" \
     --tty --interactive "${TAG}" bash
-
-
-# sudo docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
-#     --file "Dockerfile" .
-
-# sudo docker run -e PYENV_VERSION='3.7.0-debug' --rm --security-opt label=disable \
-#     --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
-#     --tty --interactive "${TAG}" bash
